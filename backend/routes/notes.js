343dd5635ce00ca6c67b9a4a5ccb6f79a41c10f9
SripaliManimekala <sripalimanimekala@gmail.com>
2024-03-02 16:42:41 +0530

    create routes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -2,9 +2,25 @@ const express = require('express')
 
 const router = express.Router()
 
+// GET all
 router.get('/',(req, res)=>{
-    res.json({mssg: 'Welcome to the API'})
+    res.json({mssg: 'get all notes'})
 })
-
+// GET one by id
+router.get('/:id',(req, res)=>{
+    res.json({mssg: 'get one'})
+})
+// POST a new note
+router.post('/', (req,res) => {
+    res.json({mssg: 'post a note'})
+})
+// DELETE a note with the given id
+router.delete('/:id', (req, res) =>{
+    res.json({mssg: 'delete a note'})
+})
+// UPDATE a note with the given id
+router.patch('/:id', (req, res) =>{
+    res.json({mssg: 'update a note'})
+})
 
 module.exports = router
