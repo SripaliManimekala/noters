61e2b31aaabebdd3a782172c4e0dab2b6d8a0edc
SripaliManimekala <sripalimanimekala@gmail.com>
2024-03-02 16:26:39 +0530

    manage routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
+require('dotenv').config()
+
 const express = require('express')
+const noteRoutes  = require('./routes/notes')
+
 
 //create express app
 const app = express()
 
+//middleware
+app.use((req, res, next) =>{
+    console.log(req.path, req.method)
+    next()
+})
+
+//routes
+app.use('/api/notes', noteRoutes)
+
 //listen for reqests
-app.listen(4000, () => {
-    console.log("Server is running on port" + 4000)
+app.listen(process.env.PORT, () => {
+    console.log("Server is running on port" , process.env.PORT);
 })
